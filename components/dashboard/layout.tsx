df8997b8521b43d74fc30dc9d6e0dc400e1b04f6
Azekowka <gabjtov@gmail.com>
2025-05-09 00:19:08 +0500

    analytics nav

--- components/dashboard/layout.tsx
+++ components/dashboard/layout.tsx
@@ -107,6 +107,10 @@ function DesktopNav({ expanded, setExpanded }: { expanded: boolean; setExpanded:
           </svg>
         </ExpandableNavItem>
 
+        <ExpandableNavItem expanded={expanded} href="/dashboard/analytics" label="Analytics">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chart-spline-icon lucide-chart-spline"><path d="M3 3v16a2 2 0 0 0 2 2h16"/><path d="M7 16c.5-2 1.5-7 4-7 2 0 2 3 4 3 2.5 0 4.5-5 5-7"/></svg>
+        </ExpandableNavItem>
+
       </nav>
     </aside>
   );
@@ -194,6 +198,15 @@ function MobileNav() {
             </svg>
             Create a link
           </Link>
+
+          <Link
+            href="/dashboard/analytics"
+            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chart-spline-icon lucide-chart-spline"><path d="M3 3v16a2 2 0 0 0 2 2h16"/><path d="M7 16c.5-2 1.5-7 4-7 2 0 2 3 4 3 2.5 0 4.5-5 5-7"/></svg>
+            Analytics
+          </Link>
+
         </nav>
       </SheetContent>
     </Sheet>
