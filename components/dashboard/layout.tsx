10b45ac6b4514c1d8ffd71515b40d417aae42f60
Azekowka <gabjtov@gmail.com>
2025-05-08 23:08:33 +0500

    minor fixes

--- components/dashboard/layout.tsx
+++ components/dashboard/layout.tsx
@@ -41,7 +41,7 @@ export default function DashboardLayout({
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <MobileNav />
             <DashboardBreadcrumb />
-            <SearchInput />
+            <div className="flex-1"></div>
             <ThemeToggle />
             <User />
           </header>
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -269,7 +269,7 @@ export function HeroSection() {
                                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                                     <Image
                                         className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                                        src="https://tailark.com//_next/image?url=%2Fmail2.png&w=3840&q=75"
+                                        src="/demophoto.png"
                                         alt="app screen"
                                         width={2700}
                                         height={1440}
@@ -277,7 +277,7 @@ export function HeroSection() {
                                     />
                                     <Image
                                         className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                                        src="https://tailark.com/_next/image?url=%2Fmail2-light.png&w=3840&q=75"
+                                        src="/demophoto.png"
                                         alt="app screen"
                                         width={2700}
                                         height={1440}
